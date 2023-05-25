5c3ffb425b9d83a49233a0d63e4d925a8ff4dcbf
Eli Pleaner <eliipleaner@gmail.com>
2023-05-25 16:08:06 -0700

    add uuid; improve interface

--- ws-do/src/components/wsMonitor.js
+++ ws-do/src/components/wsMonitor.js
@@ -1,14 +1,17 @@
 import { useCallback, useState, useEffect } from 'preact/hooks';
 
 const WSMonitor = () => {
+  const [wsInput, setWsInput] = useState('');
   const [wsUrl, setWsUrl] = useState('');
   const [ws, setWs] = useState(null);
   const [wsData, setWsData] = useState({});
+  const [id, setId] = useState(null);
 
-  const onMessage = useCallback(
-    async (event) => setWsData(await JSON.parse(event.data)),
-    []
-  );
+  const onMessage = useCallback(async (event) => {
+    const data = await JSON.parse(event.data);
+    if (data.id) setId(data.id);
+    else setWsData(data);
+  }, []);
 
   useEffect(() => {
     if (!window) return;
@@ -16,7 +19,7 @@ const WSMonitor = () => {
     const protocol = window.location.protocol.includes('https') ? 'wss' : 'ws';
     const url = `${protocol}://${location.hostname}`;
 
-    setWsUrl(url);
+    setWsInput(url);
   }, []);
 
   useEffect(() => {
@@ -26,7 +29,7 @@ const WSMonitor = () => {
 
     const wsServer = new WebSocket(wsUrl);
 
-    console.log('ws server:', wsServer);
+    console.log('ws connection:', wsServer);
     wsServer.onmessage = onMessage;
 
     setWs(wsServer);
@@ -34,19 +37,28 @@ const WSMonitor = () => {
 
   return (
     <div>
-      <input
-        type='text'
-        value={wsUrl}
-        onInput={(e) => {
-          console.log(e);
-          setWsUrl(e.target.value);
-        }}
-      />
-      <div>ws url: {wsUrl}</div>
-      <div>rss: {wsData.rss}</div>
-      <div>heap total: {wsData.heapTotal}</div>
-      <div>heap used: {wsData.heapUsed}</div>
-      <div>external: {wsData.external}</div>
+      <div>
+        <input
+          type='text'
+          value={wsInput}
+          onInput={(e) => {
+            console.log(e);
+            setWsInput(e.target.value);
+          }}
+        />
+        <button onClick={() => setWsUrl(wsInput)}>go</button>
+      </div>
+      {id && (
+        <>
+          <div>ws url: {wsUrl}</div>
+          <div>id: {id}</div>
+
+          <div>rss: {wsData.rss}</div>
+          <div>heap total: {wsData.heapTotal}</div>
+          <div>heap used: {wsData.heapUsed}</div>
+          <div>external: {wsData.external}</div>
+        </>
+      )}
     </div>
   );
 };
--- ws-do/wss.js
+++ ws-do/wss.js
@@ -3,6 +3,7 @@ const path = require('path');
 const { createServer } = require('http');
 
 const WebSocket = require('ws');
+const uuid = require('uuid');
 
 const PORT = process.env.PORT || 8081;
 
@@ -17,15 +18,39 @@ const wss = new WebSocket.WebSocketServer({
 });
 
 wss.on('connection', (ws) => {
-  const id = setInterval(() => {
+  ws.id = uuid.v4();
+  console.log(`new connection! (id ${ws.id}) | ${wss.clients.size} clients`);
+
+  ws.send(JSON.stringify({ type: 'id', id: ws.id }));
+
+  const heartbeatId = setInterval(() => {
     ws.send(JSON.stringify(process.memoryUsage()), () => null);
   }, 100);
 
-  console.log('started client interval');
+  wss.clients.forEach((client) => {
+    if (client !== ws && client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify({ type: 'clientConnected', id: ws.id }));
+    }
+  });
+
+  ws.on('message', (data, isBinary) => {
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
 
   ws.on('close', () => {
-    console.log('stopping client interval');
-    clearInterval(id);
+    console.log(`connection closed | ${wss.clients.size} clients`);
+
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({ type: 'clientDisconnected', id: ws.id }));
+      }
+    });
+
+    clearInterval(heartbeatId);
   });
 });
 
