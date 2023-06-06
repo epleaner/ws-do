6d7cdbdd5ab41937ada32e784da15948b4be819a
Eli Pleaner <eliipleaner@gmail.com>
2023-06-06 16:48:31 -0700

    Add tailwindcss

--- ws-do/src/components/header/index.js
+++ ws-do/src/components/header/index.js
@@ -3,7 +3,10 @@ import style from './style.css';
 const Header = () => (
   <header class={style.header}>
     <a href='/' class={style.logo}>
-      <h1>WS</h1>
+      <h1>monitor</h1>
+    </a>
+    <a href='/synth' class={style.logo}>
+      <h1>synth</h1>
     </a>
   </header>
 );
