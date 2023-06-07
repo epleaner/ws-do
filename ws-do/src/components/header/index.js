7458a517ce4654a2e9ded652335999433dd1ac21
Eli Pleaner <eliipleaner@gmail.com>
2023-06-07 10:28:23 -0700

    styling

--- ws-do/src/components/header/index.js
+++ ws-do/src/components/header/index.js
@@ -1,11 +1,13 @@
-import style from './style.css';
-
 const Header = () => (
-  <header class={style.header}>
-    <a href='/' class={style.logo}>
+  <header className='items-center fixed w-full h-10 z-50 flex bg-slate-800 shadow-xl gap-4'>
+    <a
+      href='/'
+      className='hover:bg-slate-700 transition-colors h-full flex items-center px-4'>
       <h1>monitor</h1>
     </a>
-    <a href='/synth' class={style.logo}>
+    <a
+      href='/synth'
+      className='hover:bg-slate-700 transition-colors h-full flex items-center px-4'>
       <h1>synth</h1>
     </a>
   </header>
